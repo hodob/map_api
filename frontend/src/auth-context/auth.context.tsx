9da9b51f7bf681a133734b536fba20fac1e0030e
지정호 <hodob76@gmail.com>
2023-06-13 11:03:46 +0900

    리펙토링

--- frontend/src/auth-context/auth.context.tsx
+++ frontend/src/auth-context/auth.context.tsx
@@ -5,7 +5,7 @@ const AuthContext = createContext<any>(null);
 
 export const AuthProvider= ({ userData, children }:{userData:string | null, children :React.ReactNode}) => {
   let [user, setUser] = useState(userData);
-  user = typeof user === "string" ? JSON.parse(user) : user;
+  user = user ? JSON.parse(user) : user;
 
   return <AuthContext.Provider value={{ user, setUser }}>{children}</AuthContext.Provider>;
 };
