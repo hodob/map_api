bc52ae684b0fd7e497e345e62799a69ec9819b52
지정호 <hodob76@gmail.com>
2023-06-13 11:00:01 +0900

    authProvider 추가

--- frontend/src/auth-context/auth.context.tsx
+++ frontend/src/auth-context/auth.context.tsx
@@ -1,15 +1,14 @@
-import React, {useState,createContext} from "react";
+import React, {useState,createContext,useContext} from "react";
 import PropTypes from "prop-types";
 
 const AuthContext = createContext<any>(null);
 
-
-export const AuthProvider:any|null= ({ userData, children }:{userData:any, children :any}) => {
-  let [user, setUser]:[user:null|any,setUser:null|any] = useState<any|null>(userData);
+export const AuthProvider= ({ userData, children }:{userData:any, children :React.ReactNode}) => {
+  let [user, setUser] = useState(userData);
   user = typeof user === "string" ? JSON.parse(user) : user;
 
   return <AuthContext.Provider value={{ user, setUser }}>{children}</AuthContext.Provider>;
 };
 
-export const useAuth = () => React.useContext(AuthContext);
+export const useAuth = () => useContext(AuthContext);
 
