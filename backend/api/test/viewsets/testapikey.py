11d209306a775153581eef208b8557f8d3eec89d
지정호 <hodob76@gmail.com>
2023-06-19 14:29:37 +0900

    양방향 암호화 로

--- backend/api/test/viewsets/testapikey.py
+++ backend/api/test/viewsets/testapikey.py
@@ -2,7 +2,6 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from ..permissions import HasUserAPIKey
 from api.test.model import UserAPIKey
-from django.contrib.auth import  hashers
 
 
 class TestApiKey(viewsets.ModelViewSet):
