f3e367f19e5741101a9b829b0b7f7312eff5a3f0
지정호 <hodob76@gmail.com>
2023-06-26 13:32:01 +0900

    일단 현재 위치부터

--- frontend/src/layouts/main/MainLayout.tsx
+++ frontend/src/layouts/main/MainLayout.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import KakaoMap from "./components/KakaoMap";
+import useGeoLocation from "./components/useGeolocation";
 
 function MainLayout() {
+    const location = useGeoLocation();
+    // console.log(location.coordinates)
     return (
         <>
-        <KakaoMap/>
+            <div className="App">
+                {location.loaded ? JSON.stringify(location)
+      : "Location data not available yet."}
+            </div>
+        <KakaoMap location={location.coordinates}/>
         </>
     );
 }
--- frontend/src/layouts/main/components/KakaoMap.tsx
+++ frontend/src/layouts/main/components/KakaoMap.tsx
@@ -1,26 +1,36 @@
 import React, { useEffect } from 'react';
+
+
+
 declare global {
   interface Window {
     kakao: any;
   }
 }
-const KakaoMap = () => {
+function KakaoMap (location:any) {
+  if(location){
+    console.log("1111")
+  } else (
+    console.log("2222")
+  )
 
   useEffect(() => {
+    if(location){
+      console.log("1111")
+    } else (
+      console.log("2222")
+    )
       window.kakao.maps.load(function() {
         const container :any= document.getElementById('map');
         const options :any= {
-          center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+          center: new window.kakao.maps.LatLng(35.2397951, 129.0814828),
+          // center: new window.kakao.maps.LatLng(location.location.lat, location.location.lng),
           level: 3
         };
-        const  map = new window.kakao.maps.Map(container, options);
+        const map = new window.kakao.maps.Map(container, options);
         });
 
-    // };
-
     return () => {
-      // Clean up the script when the component is unmounted
-      // document.head.removeChild(script);
     };
   }, []);
 
