42ce2aa26041ebad8bdfb14a4a13fef96006070c
지정호 <hodob76@gmail.com>
2023-06-26 10:08:39 +0900

    start

---
+++ frontend/src/layouts/test/test.tsx
@@ -0,0 +1,55 @@
+import react, {useState} from 'react';
+import Axios from "axios";
+import { useAuth } from '../../auth-context/auth.context';
+
+export default function Test() {
+    
+    let user = useAuth();
+    const [apikey, setApikey] = useState<string>("");
+    const [apikey2, setApikey2] = useState<string>("");
+    const [testresult, setTestResult] = useState<string>("");
+
+    // const [value, setValue] = useState<string>("");
+
+    const hangapikeychange = (event:any) => {
+        setApikey2(event.target.value);
+    };
+    const CreateApiCall = async () => {
+        try {
+            const response = await Axios.get('http://localhost:8000/api/users/apis', { headers: { Authorization: `${user?.token}` }});
+            console.log("response >>", response.data);
+            setApikey(response.data.key);
+            setApikey2(response.data.key);
+        } catch(err) {
+            console.log("Error >>", err);
+        }
+    }
+
+    const TestApiCall = async () => {
+        try {
+            const response = await Axios.get('http://localhost:8000/api/test/testapikey?apikey='+apikey2, { headers: { Authorization: `${user?.token}` }});
+            console.log("response >>", response);
+            setTestResult(response.data);
+
+        } catch(err) {
+            setTestResult("테스트 실패");
+            console.log("Error >>", err);
+        }
+    }
+
+    return (
+        <div>
+            <button onClick={CreateApiCall}>apiKey발급</button>
+            <h1>apikeyselect</h1>
+            <h1>api키 : {apikey}</h1>
+
+            <div>
+            <input  type="text" value={apikey2} onChange={hangapikeychange} />
+            <p>입력된 값: {apikey2}</p>
+            <h3>접근주소: http://localhost:8000/api/test/testapikey?apikey={apikey2}</h3>
+            </div>
+            <button onClick={TestApiCall}>apiKey테스트</button>
+            <h1>테스트결과 : {testresult}</h1>
+        </div>
+    )
+}
